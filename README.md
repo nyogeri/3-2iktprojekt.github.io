b018fa99f7310eba6f552bec11a7499d10e34fa4
nyogeri <nyogerii@gmail.com>
2021-01-31 16:11:36 +0100

    festekek

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # DIY Barkácsáruház
-# Még egy sor
---
+++ scripts.js
@@ -0,0 +1,19 @@
+$(function() {
+  let $image = $('.image-wrapper img');
+
+  $image.mouseover(function() {
+    
+    // előző
+    let $actualImage = $(this);
+    if (!$actualImage.hasClass('scale-half')) {
+      return;
+    }
+
+    // aktuális kép
+    let $previousImage = $('.image-wrapper img:not(.scale-half)');
+
+    // class variálás
+    $previousImage.addClass('scale-half');
+    $actualImage.removeClass('scale-half');
+  });
+});
